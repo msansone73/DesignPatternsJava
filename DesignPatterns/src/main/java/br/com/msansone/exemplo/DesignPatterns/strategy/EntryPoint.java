d7f61c9a429d903eeb1ea5ceb67ca1c53bfc8c6f
marcio sansone <msansone@gmail.com>
2019-04-10 21:23:45 +0100

    Add Strategy Pattern.

--- DesignPatterns/src/main/java/br/com/msansone/exemplo/DesignPatterns/strategy/EntryPoint.java
+++ DesignPatterns/src/main/java/br/com/msansone/exemplo/DesignPatterns/strategy/EntryPoint.java
@@ -3,7 +3,12 @@ package br.com.msansone.exemplo.DesignPatterns.strategy;
 public class EntryPoint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Dog toto = new Dog();
+		System.out.println(toto.getFlyingType().fly());
+		
+		Duck tale = new Duck();
+		System.out.println(tale.getFlyingType().fly());
 
 	}
 
